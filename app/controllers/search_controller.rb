889a987ee718c54781239f0f7a73fedea5220c6c
Farstep <yazawanaoki837@gmail.com>
2020-05-30 19:17:22 +0900

    init

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,18 @@
+class SearchController < ApplicationController
+
+  def search
+    if postal_code = params[:postal_code]
+      params = URI.encode_www_form({zipcode: postal_code})
+      uri = URI.parse("http://zipcloud.ibsnet.co.jp/api/search?#{params}")
+      response = Net::HTTP.get_response(uri)
+      result = JSON.parse(response.body)
+      if result["results"]
+        @zipcode = result["results"][0]["zipcode"]
+        @address1 = result["results"][0]["address1"]
+        @address2 = result["results"][0]["address2"]
+        @address3 = result["results"][0]["address3"]
+      end
+    end
+  end
+
+end
